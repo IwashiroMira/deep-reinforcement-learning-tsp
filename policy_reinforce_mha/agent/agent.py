74392a98773ee64ab168e768129bc17a8898e2de
IwashiroMira <mirachi.r316@gmail.com>
2025-05-10 14:56:58 +0900

    mha logic add

---
+++ policy_reinforce_mha/agent/agent.py
@@ -0,0 +1,148 @@
+import sys
+import os
+# プロジェクトのルートディレクトリを追加
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from itertools import combinations
+from model.model_mha import Encoder
+from model.model_mha import Decoder
+import torch
+from torch import optim
+from utils import get_device
+from torch_geometric.data import Data
+import numpy as np
+
+
+class Agent:
+    def __init__(self, lr=1e-4, gamma=0.99):
+        self.gamma = gamma  # 割引率
+        self.lr = lr  # 学習率
+        self.memory = []
+        self.device = get_device()
+        self.encoder = Encoder().to(self.device)  # ポリシーモデルの初期化
+        self.decoder = Decoder().to(self.device)
+        self.optimizer = optim.Adam(
+            list(self.encoder.parameters()) + list(self.decoder.parameters()),
+            lr=self.lr)
+    
+    def encoder_forward(self, data):
+        # Encoder
+        self.node_embeddings, self.graph_embed = self.encoder(data)  # GNNの出力を取得
+        # print("node_embeddings.shape:", self.node_embeddings.shape)
+        # print("graph_embed.shape:", self.graph_embed.shape)
+
+    def get_action(self, visited_cities):
+        visited_mask = torch.tensor(visited_cities, dtype=torch.bool, device=self.device).unsqueeze(0)  # shape: (1, 5)
+        # print("visited_mask.shape:", visited_mask.shape)
+        # print("visited_mask:", visited_mask)
+
+        if not visited_mask.any():
+            # 全て False（＝どの都市もまだ訪問していない）
+            h_last = torch.empty(0, device=self.device)
+            h_first = torch.empty(0, device=self.device)
+            t = 0
+        else:
+            first_index = np.where(visited_cities == 1)[0][0]
+            last_index = np.argmax(visited_cities)
+            # print(f"first_index: {first_index}, last_index: {last_index}")
+            h_first = self.node_embeddings[0, first_index, :].unsqueeze(0)  # shape: (1, 128)
+            h_last = self.node_embeddings[0, last_index, :].unsqueeze(0)  # shape: (1, 128)
+            t = visited_mask.sum().item()  # 訪問済み都市の数
+        
+        probs, _ = self.decoder(self.node_embeddings, self.graph_embed, h_last, h_first, visited_mask, t)
+        # print("probs:", probs)
+        # ノードを確率的に選ぶ（探索的）
+        action = torch.multinomial(probs, num_samples=1).item()  # → int 値
+        # print("selected_city:", action)
+
+        return action, probs
+    
+    def add(self, reward, action_probs):
+        self.memory.append((reward, action_probs))
+
+    def update(self):
+        G, loss = 0, 0
+        EPS = 1e-8  # 極小値を定義
+
+        for reward, action_probs in reversed(self.memory):
+            # print(f"action_probs: {action_probs}")
+            G = reward + self.gamma * G
+            loss += -torch.log(action_probs + EPS) * G  # EPSがないとlog(0)でエラーになる
+        
+        loss = loss.sum()
+        
+        self.optimizer.zero_grad()
+        # **学習前の重みを表示**
+        # print("🔍 LSTM weights BEFORE update:")
+        # for name, param in self.pi.lstm.named_parameters():
+        #     print(f"{name}: {param.data}")
+            
+        loss.backward()
+        self.optimizer.step()
+
+        # **学習後の重みを表示**
+        # print("🔍 LSTM weights AFTER update:")
+        # for name, param in self.pi.lstm.named_parameters():
+        #     print(f"{name}: {param.data}")
+            
+        self.memory = []
+    
+    # modelを保存する関数
+    def save_model(self, path):
+        # 保存
+        torch.save({
+            'encoder': self.encoder.state_dict(),
+            'decoder': self.decoder.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        }, path)
+    
+    # modelをロードする関数
+    def load_model(self, path):
+        checkpoint = torch.load(path, map_location=self.device)
+        
+        self.encoder.load_state_dict(checkpoint['encoder'])
+        self.decoder.load_state_dict(checkpoint['decoder'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+        self.encoder.to(self.device)
+        self.decoder.to(self.device)    
+
+def main():
+    agent = Agent()
+
+    device = get_device()
+
+    state = torch.tensor([
+        [95.02370691, 54.86014805],
+        [79.64110929, 9.31860416],
+        [37.19301454, 14.77507159],
+        [62.41494659, 80.49261453],
+        [28.36552228, 90.45835087]
+    ], dtype=torch.float32)
+
+    # 都市数
+    num_nodes = state.shape[0]
+
+    # すべての都市ペアを生成（i ≠ j）
+    edges = list(combinations(range(num_nodes), 2))
+
+    # edge_index の作成
+    edge_index = torch.tensor(edges, dtype=torch.long).T  # 転置して (2, N) の形にする
+
+    # ユークリッド距離を計算してエッジの重みとする
+    edge_weight = torch.norm(state[edge_index[0]] - state[edge_index[1]], dim=1)
+
+    # Min-Maxスケーリングを追加 (0~1に正規化)
+    edge_weight = (edge_weight - edge_weight.min()) / (edge_weight.max() - edge_weight.min())
+
+    # `torch_geometric.data.Data` オブジェクト作成
+    data = Data(x=state.to(device), edge_index=edge_index.to(device), edge_weight=edge_weight.to(device))
+    
+    visited_cities = np.array([0, 2, 1, 0, 3])  # 各都市の訪問ステップ
+    agent.encoder_forward(data)
+    agent.get_action(visited_cities)
+    # nex_node, probs = agent.get_action(data, state)
+    # print('next_node:', nex_node)
+    # print('probs:', probs)
+
+if __name__ == '__main__':
+    main()
---
+++ policy_reinforce_mha/inference.py
@@ -0,0 +1,191 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+# プロジェクトのルートディレクトリを追加
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from env.env import TSPEnv
+from agent.agent import Agent
+import json
+import argparse
+import sys
+
+
+# input_data = np.array([
+#     [40,57],
+#     [51,28],
+#     [57,  3],
+#     [45, 94],
+#     [58, 28],
+# ])
+input_data = np.array([
+    [34, 83],
+    [54,  0],
+    [21,  5],
+    [ 4, 63],
+    [59, 55],
+    [24, 89],
+    [75, 83],
+    [ 6, 94],
+    [52, 97],
+    [18, 85],
+    [35, 77],
+    [33, 97],
+    [11, 23],
+    [24, 71],
+    [ 9, 98],
+    [31, 66],
+    [98, 89],
+    [74, 59],
+    [11, 29],
+    [50, 83],
+    [10, 78],
+    [81, 83],
+    [ 2, 75],
+    [48, 37],
+    [43, 86]
+])
+# input_data = np.array([
+#     [34, 83],
+#     [54,  0],
+#     [21,  5],
+#     [ 4, 63],
+#     [59, 55],
+#     [24, 89],
+#     [75, 83],
+#     [ 6, 94],
+#     [52, 97],
+#     [18, 85],
+# ])
+
+def plot_route(reward_history):
+    # 最小トータルリワードのエピソードを取得
+    best_episode = max(reward_history, key=lambda x: x["total_reward"])
+    best_reward = best_episode["total_reward"]
+    best_order = best_episode["visit_orders"]
+
+
+    # 訪問順序の最初と最後に 0 を追加して巡回経路にする
+    best_order = [0] + best_order + [0]
+
+    # 訪問順序に基づく座標を取得
+    ordered_coords = [input_data[i] for i in best_order]
+
+    # プロットの作成
+    plt.figure(figsize=(6, 6))
+    plt.scatter(input_data[:, 0], input_data[:, 1], color='red', label='Cities')
+    plt.plot(*zip(*ordered_coords), marker='o', linestyle='-', color='blue', label='Best Route')
+
+    # 各都市にラベルを付与
+    for i, (x, y) in enumerate(input_data):
+        plt.text(x, y, str(i), fontsize=12, verticalalignment='bottom', horizontalalignment='right')
+
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.title(f'Best Route with Total Reward: {-1 * best_reward}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_reward_history(reward_history_rl, reward_history_random):
+    plt.figure(figsize=(10, 6))
+    plt.plot(reward_history_rl, label='RL', linestyle='-', marker='o')
+    plt.plot(reward_history_random, label='Random', linestyle='--', marker='x')
+    
+    plt.xlabel('Episodes')
+    plt.ylabel('Total Reward')
+    plt.title('Total Reward History')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def calc_distance_random_order(input_data):
+    # 訪問順序をランダムに生成
+    n_cities = len(input_data)
+    visit_orders = np.random.permutation(n_cities)
+    visit_orders = np.append(visit_orders, visit_orders[0])  # 最初の都市に戻る
+
+    # 訪問順序に基づく座標を取得
+    ordered_coords = [input_data[i] for i in visit_orders]
+
+    # 総移動距離の計算
+    total_distance = 0
+    for i in range(n_cities):
+        total_distance += np.linalg.norm(ordered_coords[i] - ordered_coords[i + 1])
+
+    return total_distance
+
+
+def main(model_path='save/model.pth', episodes=100, plot=False):
+    # 同名の .json ファイルからパラメータ読み込み
+    config_path = model_path.replace(".pth", ".json")
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    # config を使ってAgent構築
+    agent = Agent(
+        hidden_size=config["hidden_size"],
+        lr=config["lr"],
+        gamma=config["gamma"]
+    )
+    env = TSPEnv(fixed_coords=input_data)
+    reward_history = []
+    reward_history_random = []
+
+    # 保存済みモデルをロード
+    agent.load_model(model_path)
+
+    for episode in range(episodes):
+        # print('episode:', episode)
+        data, state = env.reset()
+        done = False
+        total_reward = 0
+        visit_orders = []
+
+        while not done:
+            action, probs = agent.get_action(data, state)
+            visit_orders.append(action)
+            next_data, next_state, reward, done = env.step(action)
+            
+            total_reward += reward
+            state = next_state
+            data = next_data
+
+        reward_history.append({
+            "total_reward": total_reward,
+            "visit_orders": visit_orders
+            })
+
+        # print(f'Episode: {episode}, Total Reward: {total_reward}')
+        # print(f'visit_orders:{visit_orders}')
+
+        # ランダムな訪問順序の総移動距離を計算
+        total_distance_random = calc_distance_random_order(input_data)
+        reward_history_random.append(total_distance_random)
+
+        # print(f'Episode: {episode}, Total Reward Random: {total_distance_random}')
+    
+    # データの準備
+    reward_history_rl = [-1 * r["total_reward"] for r in reward_history]
+    reward_history_random = reward_history_random
+
+    # Best Modelでグラフをプロット
+    if plot:
+        plot_route(reward_history)  # 最短経路のプロット
+        plot_reward_history(reward_history_rl, reward_history_random)
+    
+
+    # best_reward = min(reward_history_rl)
+    best_reward = np.mean(reward_history_rl) 
+    print(best_reward)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_path", nargs='?', default="save/model.pth")
+    parser.add_argument("--plot", action="store_true", help="プロットを表示するかどうか")
+    parser.add_argument("--episodes", type=int, default=100)
+    args = parser.parse_args()
+
+    main(args.model_path, args.episodes, plot=args.plot)
