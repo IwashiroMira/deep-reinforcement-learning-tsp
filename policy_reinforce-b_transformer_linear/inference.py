b47614739a2f18e90a3f77c404a47c8a89d2a179
IwashiroMira <mirachi.r316@gmail.com>
2025-08-01 23:03:45 +0900

    ランダムサンプルと貪欲法で推論できるように改修

--- policy_reinforce-b_transformer_linear/inference.py
+++ policy_reinforce-b_transformer_linear/inference.py
@@ -7,10 +7,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from env.env import TSPEnv
 from agent.agent import Agent
-import json
+from agent.baseline import BaseLine
+import copy
 import argparse
 import sys
 from config import environment, inference
+import utils
 
 # n=48
 # input_data = np.array([
@@ -96,139 +98,116 @@ input_data = np.array([
 # B×N×D に reshape（B=1）
 input_data = input_data[np.newaxis, :, :]  # shape = (1, 25, 2)
 
-def plot_route(reward_history):
-    # 最小トータルリワードのエピソードを取得
-    best_episode = max(reward_history, key=lambda x: x["total_reward"])
-    best_reward = best_episode["total_reward"]
-    best_order = best_episode["visit_orders"]
-    visit_order_list = [v.item() for v in best_order]
-    print(f"visit_order_list: {visit_order_list}")
-    print(f'best_reward: {best_reward}')
-
-    # 訪問順序の最初と最後に 0 を追加して巡回経路にする
-    best_order.append(best_order[0])
-    
-    # バッチ次元を除いて扱う
-    input_coords = input_data[0]  # shape: (25, 2)
-    
-    # 訪問順序に基づく座標を取得
-    ordered_coords = [input_coords[i] for i in best_order]
-
-    # プロットの作成
-    plt.figure(figsize=(6, 6))
-    plt.scatter(input_coords[:, 0], input_coords[:, 1], color='red', label='Cities')
-    plt.plot(*zip(*ordered_coords), marker='o', linestyle='-', color='blue', label='Best Route')
-
-    # 各都市にラベルを付与
-    for i, (x, y) in enumerate(input_coords):
-        plt.text(x, y, str(i), fontsize=12, verticalalignment='bottom', horizontalalignment='right')
-
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
-    plt.title(f'Best Route with Total Reward: {-1 * best_reward}')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"./route/Best Route")
-    plt.close()  # 状態をリセット（重要）
-
-def plot_reward_history(reward_history_rl, reward_history_random):
-    plt.figure(figsize=(10, 6))
-    plt.plot(reward_history_rl, label='RL', linestyle='-', marker='o')
-    plt.plot(reward_history_random, label='Random', linestyle='--', marker='x')
-    
-    plt.xlabel('Episodes')
-    plt.ylabel('Total Reward')
-    plt.title('Total Reward History')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"./route/Best Route Reward History")
-    plt.close()  # 状態をリセット（重要）
-
-def calc_distance_random_order(input_data):
-    # 訪問順序をランダムに生成
-    n_cities = len(input_data)
-    visit_orders = np.random.permutation(n_cities)
-    visit_orders = np.append(visit_orders, visit_orders[0])  # 最初の都市に戻る
-
-    # 訪問順序に基づく座標を取得
-    ordered_coords = [input_data[i] for i in visit_orders]
-
-    # 総移動距離の計算
-    total_distance = 0
-    for i in range(n_cities):
-        total_distance += np.linalg.norm(ordered_coords[i] - ordered_coords[i + 1])
-
-    return total_distance
-
-
 def main(model_path='save/model.pth', episodes=100, plot=True):
     # 同名の .json ファイルからパラメータ読み込み
-    config_path = model_path.replace(".pth", ".json")
-    with open(config_path, "r") as f:
-        config = json.load(f)
+    if os.path.exists(model_path):
+        config = utils.load_config(model_path)
 
-    # config を使ってAgent構築
+    # config を使ってAgentとbaseline構築
     agent = Agent(
         lr=config["lr"],
         gamma=config["gamma"]
     )
-    env = TSPEnv(batch_size=inference["batch_size"], n_cities=environment["num_cities"], fixed_coords=input_data)
+    baseline = BaseLine(
+        lr=config["lr"],
+        gamma=config["gamma"]
+    )
+
+    # agent用の環境を初期化
+    env = TSPEnv(
+        batch_size=inference["batch_size"], 
+        n_cities=environment["num_cities"], 
+        fixed_coords=input_data
+    )
     env.generate_coords()
-    reward_history = []
+    # baseline用の環境を初期化
+    baseline_env = TSPEnv(
+        batch_size=inference["batch_size"], 
+        n_cities=environment["num_cities"], 
+        fixed_coords=input_data
+    )
+    baseline_env.generate_coords()
+
+    reward_history_greedy = []
     reward_history_random = []
 
     # 保存済みモデルをロード
     agent.load_model(model_path)
+    baseline.model.load_state_dict(copy.deepcopy(agent.model.state_dict()))
 
     for episode in range(episodes):
         # print('episode:', episode)
+        # 1. AgentとBaselineの環境をリセット
         data, visited_cities = env.reset()
+        baseline_data, visited_cities_baseline = baseline_env.reset()        
+
+        # 2. encoder_forwardを実行
         agent.encoder_forward(data)
-        done = False
-        total_reward = 0
-        visit_orders = []
+        baseline.encoder_forward(baseline_data)
 
-        while not done:
+        # doneフラグの初期化
+        agent_done = False
+        baseline_done = False
+
+        random_reward = 0
+        random_visit_orders = []
+        greedy_reward = 0
+        greedy_visit_orders = []
+        
+        # agentの推論
+        while not agent_done:
             action, probs = agent.get_action(visited_cities)
-            visit_orders.append(action)
-            next_visited_cities, reward, done = env.step(action)
+            random_visit_orders.append(action)
+            next_visited_cities, reward, agent_done = env.step(action)
             
-            total_reward += reward
+            random_reward += reward
             visited_cities = next_visited_cities
+        
+        # baselineの推論
+        while not baseline_done:
+            action, probs = baseline.get_action(visited_cities_baseline)
+            greedy_visit_orders.append(action)
+            next_visited_cities, reward, baseline_done = baseline_env.step(action)
+            
+            greedy_reward += reward
+            visited_cities_baseline = next_visited_cities
+        
+        # ランダムサンプリングの結果を保存
+        reward_history_random.append({
+            "total_reward": random_reward,
+            "visit_orders": random_visit_orders
+        })
 
-        reward_history.append({
-            "total_reward": total_reward,
-            "visit_orders": visit_orders
-            })
+        # greedyサンプリングの結果を保存
+        reward_history_greedy.append({
+            "total_reward": greedy_reward,
+            "visit_orders": greedy_visit_orders
+        })
 
-        # print(f'Episode: {episode}, Total Reward: {total_reward}')
-        # print(f'visit_orders:{visit_orders}')
-
-        # ランダムな訪問順序の総移動距離を計算
-        total_distance_random = calc_distance_random_order(input_data)
-        reward_history_random.append(total_distance_random)
-
-        # print(f'Episode: {episode}, Total Reward Random: {total_distance_random}')
-    
     # データの準備
-    reward_history_rl = [-1 * r["total_reward"] for r in reward_history]
-    reward_history_random = reward_history_random
+    random_reward_results = [-1 * r["total_reward"] for r in reward_history_random]
+    greedy_reward_results = [-1 * r["total_reward"] for r in reward_history_greedy]
 
     # Best Modelでグラフをプロット
     if plot:
-        plot_route(reward_history)  # 最短経路のプロット
-        plot_reward_history(reward_history_rl, reward_history_random)
+        utils.plot_route(input_data, reward_history_random, "random")  # ランダムサンプリングの最短経路のプロット
+        utils.plot_route(input_data, reward_history_greedy, "greedy")  # Greedyサンプリングの最短経路のプロット
+        utils.plot_reward_history(random_reward_results, greedy_reward_results)
 
-    min_reward = min(reward_history_rl)  # 最小値
-    # mean_reward = np.mean(reward_history_rl) 
-    print(float(min_reward.item()))  # 文字列入れない、optunaでエラーになる
+    random_min_reward = min(random_reward_results)  # ランダムサンプリングの最小値
+    greedy_min_reward = min(greedy_reward_results)  # Greedyサンプリングの最小値
+    print(f"Random Sampling Min Reward: {random_min_reward}")
+    print(f"Greedy Sampling Min Reward: {greedy_min_reward}")
+    
+    # optuna対応
+    # print(float(min_reward.item()))  # 文字列入れない、optunaでエラーになる
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("model_path", nargs='?', default="save/model.pth")
     parser.add_argument("--plot", action="store_true", help="プロットを表示するかどうか")
-    parser.add_argument("--episodes", type=int, default=10000)
+    parser.add_argument("--episodes", type=int, default=1000)
     args = parser.parse_args()
 
     main(args.model_path, args.episodes, plot=args.plot)
--- policy_reinforce-b_transformer_linear/utils.py
+++ policy_reinforce-b_transformer_linear/utils.py
@@ -1,7 +1,9 @@
 import torch
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
-
+@staticmethod
 def get_device():    
     # デバイスの定義 (GPUが利用可能であればGPUを使う)
     if torch.backends.mps.is_available():  # M1/M2チップのMetal API（GPU）対応
@@ -12,7 +14,7 @@ def get_device():
         device = torch.device("cpu")  # CPU
     return device
 
-
+@staticmethod
 def save_config(lr, gamma, save_path):
     """
     設定をJSON形式で保存する関数
@@ -24,6 +26,87 @@ def save_config(lr, gamma, save_path):
     json_path = save_path.replace(".pth", ".json")
     with open(json_path, "w") as f:
         json.dump(config, f)
+
+@staticmethod
+def load_config(model_path):
+    """
+    モデルの設定をJSONファイルから読み込む関数
+    :param model_path: モデルのパス（.pthファイル）
+    :return: 設定辞書
+    """
+    config_path = model_path.replace(".pth", ".json")
+    with open(config_path, "r") as f:
+        config = json.load(f)
+    return config
+
+@staticmethod
+def plot_route(input_data, reward_history, type):
+    # 最小トータルリワードのエピソードを取得
+    best_episode = max(reward_history, key=lambda x: x["total_reward"])
+    best_reward = best_episode["total_reward"]
+    best_order = best_episode["visit_orders"]
+    visit_order_list = [v.item() for v in best_order]
+    print(f"visit_order_list: {visit_order_list}")
+    print(f'best_reward: {best_reward}')
+
+    # 訪問順序の最初と最後に 0 を追加して巡回経路にする
+    best_order.append(best_order[0])
+    
+    # バッチ次元を除いて扱う
+    input_coords = input_data[0]  # shape: (25, 2)
+    
+    # 訪問順序に基づく座標を取得
+    ordered_coords = [input_coords[i] for i in best_order]
+
+    # プロットの作成
+    plt.figure(figsize=(6, 6))
+    plt.scatter(input_coords[:, 0], input_coords[:, 1], color='red', label='Cities')
+    plt.plot(*zip(*ordered_coords), marker='o', linestyle='-', color='blue', label='Best Route')
+
+    # 各都市にラベルを付与
+    for i, (x, y) in enumerate(input_coords):
+        plt.text(x, y, str(i), fontsize=12, verticalalignment='bottom', horizontalalignment='right')
+
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.title(f'Best Route with Total Reward {type}: {-1 * best_reward}')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"./route/Best Route {type}")
+    plt.close()  # 状態をリセット（重要）
+
+@staticmethod
+def plot_reward_history(random_reward_history, greedy_reward_history):
+    plt.figure(figsize=(10, 6))
+    plt.plot(random_reward_history, label='random', linestyle='-', marker='o')
+    plt.plot(greedy_reward_history, label='greedy', linestyle='--', marker='x')
+    
+    plt.xlabel('Episodes')
+    plt.ylabel('Total Reward')
+    plt.title('Total Reward History')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"./route/Best Route Reward History")
+    plt.close()  # 状態をリセット（重要）
+
+@staticmethod
+def calc_distance_random_order(input_data):
+    # 訪問順序をランダムに生成
+    n_cities = len(input_data)
+    visit_orders = np.random.permutation(n_cities)
+    visit_orders = np.append(visit_orders, visit_orders[0])  # 最初の都市に戻る
+
+    # 訪問順序に基づく座標を取得
+    ordered_coords = [input_data[i] for i in visit_orders]
+
+    # 総移動距離の計算
+    total_distance = 0
+    for i in range(n_cities):
+        total_distance += np.linalg.norm(ordered_coords[i] - ordered_coords[i + 1])
+
+    return total_distance
+
+
 
 
 
